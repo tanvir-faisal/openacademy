c7b21fc465f9464c18e48b44b8d20bfb81054f72
tanvir <tanvirfaisal10@gmail.com>
2016-11-07 15:01:18 +0600

    odoo demo me

---
+++ __openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """
+       Summary Test""",
+
+    'description': """
+        
+    """,
+
+    'author': "Tanvir",
+    'website': "http://www.BayIT.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'ModuleTest',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        'views/templates.xml',
+        'views/openacademy.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
